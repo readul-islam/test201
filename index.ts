49ec803d5ff711bbe4f69aaf95b8e0f66506d37a
readul-islam <developer.readul@gmail.com>
2025-01-01 13:41:59 +0600

    ddd

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
 console.log("any test will be ");
+
+var a: string = `
+The brown fox jumps over the lazy dog.the brown fox jumps over the lazy dog. the brown fox jumps over the lazy dog.the brown fox jumps over the lazy dog. the brown fox jumps over the lazy dog. the brown fox jumps over the lazy dog. the brown fox jumps over the lazy dog. the brown fox jumps over the lazy dog. the brown fox jumps over the lazy dog. the brown fox jumps over the lazy dog.
+
+const readul = <Sonar/> <Bangla> ami tomai balobbasi chiroding </Bangla>
+`;
